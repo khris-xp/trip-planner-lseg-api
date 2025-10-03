5f91d1f25f81d1e01ab21cea607b5237bfa57ec9
Umbs01 <overpowerpotato0@gmail.com>
2025-10-03 15:27:17 +0700

    update dto

--- models/travelPlan.py
+++ models/travelPlan.py
@@ -1,23 +1,39 @@
-from pydantic import BaseModel
 from datetime import time as dt_time
+from datetime import datetime
 from typing import Optional, List
-import json
+
+from pydantic import BaseModel, model_validator
 
 
 class TravelPlanRequest(BaseModel):
     day: int
     country: str
-    startTime: Optional[dt_time] = None
-    endTime: Optional[dt_time] = None
+    start_time: Optional[dt_time] = None
+    end_time: Optional[dt_time] = None
+
+    @model_validator()
+    def check_times(self):
+        if self.start_time and self.end_time and self.start_time >= self.end_time:
+            raise ValueError("start_time must be before end_time")
+        if self.day < 1 and self.day > 31:
+            raise ValueError("day must be between 1 and 31")
+        return self
 
 
 class DayPlan(BaseModel):
-    day: int
-    time: str
-    start: Optional[dt_time] = None
-    end: Optional[dt_time] = None
-    detials: str
+    time: dt_time
+    start: datetime = None
+    end: datetime = None
+    details: str
+
+    @model_validator()
+    def check_range(self):
+        if (self.start and not self.end) or (self.end and not self.start):
+            raise ValueError("Both start and end must be provided together")
+        if self.start and self.end and self.start >= self.end:
+            raise ValueError("start must be before end")
+        return self
 
 
 class TravelPlanResponse(BaseModel):
-    __root__: List[DayPlan]
+    plan: List[DayPlan]
